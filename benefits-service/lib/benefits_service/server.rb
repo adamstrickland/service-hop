02f03046f4217101dbd6bd8d31160188687e9f04
Adam Strickland <dev+adamstrickland@wellmatchhealth.com>
2015-03-26 16:23:22 -0500

    checkpoint; non-working direct comm'n

--- benefits-service/lib/benefits_service/server.rb
+++ benefits-service/lib/benefits_service/server.rb
@@ -11,7 +11,46 @@ module BenefitsService
     end
   end
 
-  class Server
+  class DirectServer
+    include ::Celluloid
+
+    finalizer :shutdown
+
+    def shutdown; end
+
+    def self.configure
+      self.new
+    end
+
+    def initialize
+      @connection = Bunny.new("amqp://localhost:5672")
+      @connection.start
+
+      @channel = @connection.create_channel
+
+      @exchange = @channel.default_exchange
+
+      queue = @channel.queue("benefits-direct")
+      puts "subscribing"
+      queue.subscribe(block: true) do |di, m, p|
+        puts "message received"
+        corr_id = di.routing_key.split(/\./).last
+        message = "OK #{corr_id}"
+        puts message
+        @exchange.publish("REPLY #{corr_id}", routing_key: m.reply_to, correlation_id: m.correlation_id)
+      end
+
+      super
+    end
+
+    def start
+      loop {
+        sleep(1)
+      }
+    end
+  end
+
+  class PubSubServer
     include ::Celluloid
 
     finalizer :shutdown
@@ -48,4 +87,7 @@ module BenefitsService
       }
     end
   end
+
+  # class Server < PubSubServer; end
+  class Server < DirectServer; end
 end
--- the-api/the_api.rb
+++ the-api/the_api.rb
@@ -14,44 +14,98 @@ module TheApi
     end
   end
 
-  class Version2 < Grape::API
-    version 'v2', using: :accept_version_header
+  module Version2
+    module Hoppy
+      def self.included(base)
+        base.send(:version, 'v2', using: :accept_version_header)
+        base.send(:helpers, InstanceMethods)
+      end
 
-    resource :benefits do
-      get do
-        conn = Bunny.new "amqp://localhost:5672"
-        conn.start
-        channel = conn.create_channel
-        exchange = channel.topic "benefits", auto_delete: true, durable: true
-        queue = channel.queue("benefits", auto_delete: true, durable: false)
-        corr_id = SecureRandom.uuid
-        @result = nil
-
-        begin
-          key = "response.#{corr_id}"
-          puts "binding to #{key}"
-          binding = queue.bind(exchange, routing_key: key)
-          consumer = binding.subscribe do|di, m, p|
-            puts "RCVD #{m} | #{p}"
-            @result = p
+      module InstanceMethods
+        def connection
+          unless @connection.present?
+            @connection = Bunny.new "amqp://localhost:5672"
+            @connection.start
           end
-          puts "consumer: #{consumer}"
-          puts "current result: #{@result}"
+          @connection
+        end
 
-          exchange.publish("", routing_key: "request.#{corr_id}")
+        def channel
+          @channel ||= connection.create_channel
+        end
+      end
+    end
 
-          unless @result.present?
-            loop do
-              sleep(0.2)
-              puts "tick #{corr_id}: result \"#{@result}\""
-              break if @result.present?
+    class Direct < Grape::API
+      include Hoppy
+
+      resource :benefits do
+        get do
+          exchange = channel.default_exchange
+          corr_id = SecureRandom.uuid
+
+          that = Class.new do
+            attr_accessor :response, :correlation_id
+            attr_reader :lock, :condition
+
+            def initialize
+              @lock = Mutex.new
+              @condition = ConditionVariable.new
+            end
+          end.new
+
+          reply_queue = channel.queue("", :exclusive => true)
+          reply_queue.subscribe do |di, m, p|
+            if m[:correlation_id] == that.correlation_id
+              that.response = p
+              that.lock.synchronize{ that.condition.signal }
             end
           end
-          return @result
-        ensure
-          queue.unbind(exchange)
 
+          puts "publishing #{corr_id}"
+          exchange.publish("benefits-direct", routing_key: "request.#{corr_id}", correlation_id: corr_id, reply_to: reply_queue.name)
+          that.lock.synchronize{ that.condition.wait(that.lock) }
+          that.response
+        end
+      end
+    end
+
+    class PubSub < Grape::API
+      include Hoppy
+
+      resource :benefits do
+        get do
+          exchange = channel.topic "benefits", auto_delete: true, durable: true
+          queue = channel.queue("benefits", auto_delete: true, durable: false)
+          corr_id = SecureRandom.uuid
           @result = nil
+
+          begin
+            key = "response.#{corr_id}"
+            puts "binding to #{key}"
+            binding = queue.bind(exchange, routing_key: key)
+            consumer = binding.subscribe do|di, m, p|
+              puts "RCVD #{m} | #{p}"
+              @result = p
+            end
+            puts "consumer: #{consumer}"
+            puts "current result: #{@result}"
+
+            exchange.publish("", routing_key: "request.#{corr_id}")
+
+            unless @result.present?
+              loop do
+                sleep(0.2)
+                puts "tick #{corr_id}: result \"#{@result}\""
+                break if @result.present?
+              end
+            end
+            return @result
+          ensure
+            queue.unbind(exchange)
+
+            @result = nil
+          end
         end
       end
     end
@@ -72,7 +126,8 @@ module TheApi
   class API < Grape::API
     include ApiSupport
     mount TheApi::Version1
-    mount TheApi::Version2
+    # mount TheApi::Version2::PubSub
+    mount TheApi::Version2::Direct
   end
 
 end
